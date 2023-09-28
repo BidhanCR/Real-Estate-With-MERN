cac107aff6c338673634886dbb5eebe8e8981754
BidhanCR <bidhanroy300805@gmail.com>
2023-09-28 13:56:04 +0600

    create a middleware and a function to handle possible errors

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,16 +1,16 @@
-import User from "../models/user.model.js"
-import bcrypt from "bcryptjs"
-export const signup = async(req, res) => {
-const {username, email, password} = req.body;
-const hashPassword = bcrypt.hashSync(password, 10);
-const newUser = new User({username, email, password: hashPassword});
-try {
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashPassword });
+  try {
     await newUser.save();
-res.status(201).json({
-    message: "User created Successfully"
-})
-} catch (error) {
-    res.status(500).json(error.message)
-}
-
-}
+    res.status(201).json({
+      message: "User created Successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- server/index.js
+++ server/index.js
@@ -27,5 +27,13 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-
-
+// error handler
+app.use((err, req, res, next)=> {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
+})
