fda421b3c2d21e29bfda5304a7ad78dc5814704e
BidhanCR <bidhanroy300805@gmail.com>
2023-09-28 10:58:05 +0600

    connect to database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 import express from "express";
-import 'dotenv/config'
+import "dotenv/config";
+import mongoose from "mongoose";
 const port = process.env.PORT;
+
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    console.log("DB connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
 
-app.listen(port, ()=> {
-    console.log(`Server is running on port ${port}`)
-})
-
-
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
