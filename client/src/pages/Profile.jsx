35ead95be63723d36ba1900b76a87d4865934eb4
BidhanCR <bidhanroy300805@gmail.com>
2023-09-29 13:08:48 +0600

    user update functionality completed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,18 @@
 import { useEffect, useRef, useState } from "react";
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { getStorage, uploadBytesResumable, ref, getDownloadURL } from "firebase/storage";
 import { app } from "../firebase.config";
+import { updateUserFailure, updateUserStart, updateUserSuccess } from "../redux/features/user/userSlice";
 
 const Profile = () => {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
 
   useEffect(() => {
     if (file) {
@@ -37,10 +40,40 @@ const Profile = () => {
         })
       });
   }
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+      })
+
+      const data = await res.json();
+      console.log(data)
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message))
+        return;
+      }
+
+      dispatch(updateUserSuccess(data))
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message))
+    }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*" />
         <img onClick={() => fileRef.current.click()} src={currentUser.avatar}
           alt="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" />
@@ -57,11 +90,17 @@ const Profile = () => {
             ''
           )}
         </p>
-        <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username" />
-        <input type="text" placeholder="email" className="border p-3 rounded-lg" id="email" />
-        <input type="text" placeholder="password" className="border p-3 rounded-lg" id="password" />
-        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">Update</button>
+        <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username"
+          defaultValue={currentUser.username} onChange={handleChange} />
+        <input type="text" placeholder="email" className="border p-3 rounded-lg" id="email"
+          defaultValue={currentUser.email} onChange={handleChange} />
+        <input type="text" placeholder="password" className="border p-3 rounded-lg" id="password" onChange={handleChange} />
+        <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+          {loading ? "Loading..." : "Update"}
+        </button>
       </form>
+      <p className="text-red-700 mt-5 text-center">{error ? error : ""}</p>
+      <p className="text-green-700 mt-5 text-center">{updateSuccess ? "User is updated successfully" : ""}</p>
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import "dotenv/config";
 import mongoose from "mongoose";
+import cors from "cors";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-
-
+import cookieParser from "cookie-parser";
 
 
 const port = process.env.PORT;
@@ -19,9 +19,14 @@ mongoose
   });
 
 const app = express();
+
+app.use(cookieParser());
+app.use(cors());
 app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
